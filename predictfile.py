4f79f981fdc2c26423446e3d8a62896407b8bd5c
terappy <terakei818@gmail.com>
2018-08-05 04:25:21 +0900

    アップロードした画像を表示する処理の追加

--- predictfile.py
+++ predictfile.py
@@ -56,3 +56,9 @@ def upload_file():
     </body>
     </html>
     '''
+
+from flask import send_from_directory
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
